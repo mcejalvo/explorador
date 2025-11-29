3099aaecb614e9248a9e2c16f818623ce800d7ad
Marc Cejalvo <44811751+mcejalvo@users.noreply.github.com>
2025-11-29 19:31:16 +0100

    Update query.py

--- query/query.py
+++ query/query.py
@@ -29,7 +29,7 @@ if not TOKEN:
 
 messages = []
 DATA_FILE_ID = "11XAt4ezwKcflgqTCCmSDPTDtR29kNGkK"
-USERS_DATA_FILE_ID = "14mAaP2Kr4d5T8zCrf2NV6-9qMO7dzi-iGhABoHvTTeY"
+USERS_DATA_FILE_ID = "1bPLZh4RTwuWjnJO8-BMtIdOmOwYy_vsj"
 LOG_FILE_ID = "1MigJVfJxzoOKpMrKURhmLL64stVRB_Rq"
 
 # Load the user list from Google Drive
